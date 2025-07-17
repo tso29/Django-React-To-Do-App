2d07015a2c4a1182c0536c998d4b4558f4e11b5c
Tayra <sotomezatayra@gmail.com>
2025-07-17 13:26:39 -0500

    Change import in pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
-import NotFound from './pages/notFound'
+import NotFound from './pages/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 
 function Logout() {
